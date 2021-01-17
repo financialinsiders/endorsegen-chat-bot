9612b4a595557101ea837c854ef9857a1dbd1cb8
Mohamed Sithik H <sksithik@gmail.com>
2021-01-17 03:51:01 +0000

    angular 11 upgrade

--- src/app/chat/chat-avatar/chat-avatar.component.spec.ts
+++ src/app/chat/chat-avatar/chat-avatar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ChatAvatarComponent } from './chat-avatar.component';
 
@@ -6,7 +6,7 @@ describe('ChatWidgetAvatarComponent', () => {
   let component: ChatAvatarComponent;
   let fixture: ComponentFixture<ChatAvatarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ChatAvatarComponent ]
     })
